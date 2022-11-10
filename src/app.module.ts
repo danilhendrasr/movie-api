4b68d55b6551bbd74224e5ce67720dd631d9d7ce
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-10 21:42:33 +0700

    feat: movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
