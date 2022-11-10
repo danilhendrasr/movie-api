b630cdf161c7d05149520768f375f6b400a3ac8f
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-10 21:43:32 +0700

    feat: casts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { CastsModule } from './casts/casts.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, CastsModule],
   controllers: [AppController],
   providers: [AppService],
 })
