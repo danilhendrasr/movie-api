9dfb84a88a3825b5860811dd64cbe5653dc74d0b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-11 14:17:11 +0700

    feat: utilize config module to load environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,30 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { CastsModule } from './casts/casts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envKeys, EnvTypes } from './constants';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get(envKeys.db.host),
+          port: +configService.get(envKeys.db.port),
+          username: configService.get(envKeys.db.username),
+          password: configService.get(envKeys.db.password),
+          database: configService.get(envKeys.db.name),
+          autoLoadEntities: true,
+          synchronize:
+            configService.get<EnvTypes>(envKeys.envType) === EnvTypes.DEV
+              ? true
+              : false,
+        };
+      },
     }),
     MoviesModule,
     CastsModule,
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,15 @@
-export const constants = {
-  providerNames: {
-    dataSource: 'DATA_SOURCE',
+export const envKeys = {
+  envType: 'ENV',
+  db: {
+    host: 'DB_HOST',
+    port: 'DB_PORT',
+    username: 'DB_USERNAME',
+    password: 'DB_PASSWORD',
+    name: 'DB_NAME',
   },
-};
+} as const;
+
+export enum EnvTypes {
+  DEV = 'DEV',
+  PROD = 'PROD',
+}
