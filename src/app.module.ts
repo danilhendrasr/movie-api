c70285cb752e478d9296e981062d9136721820a6
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-11 08:37:37 +0700

    feat: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { CastsModule } from './casts/casts.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [MoviesModule, CastsModule],
+  imports: [MoviesModule, CastsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.providers.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { constants } from '../constants';
+
+export const databaseProviders = [
+  {
+    provide: constants.providerNames.dataSource,
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'test',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
