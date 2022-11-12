3633502ee6c5595c2696a2620497bce0e9c0cf09
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-12 10:00:27 +0700

    feat: setup swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({ type: VersioningType.URI });
+
+  const config = new DocumentBuilder()
+    .setTitle('Movies API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
