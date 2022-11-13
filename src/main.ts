56faf3ba9993a44927df494d96292df82d123c1d
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-13 15:21:59 +0700

    feat: entity not found exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@ import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { EntityNotFoundExceptionFilter } from './shared/filters/entity-not-found-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableVersioning({ type: VersioningType.URI });
-
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Movies API')
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const app = await NestFactory.create(AppModule);
+  app.enableVersioning({ type: VersioningType.URI });
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, swaggerConfig),
+  );
 
   await app.listen(3000);
 }
